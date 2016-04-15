0eb3c0e8957e3bf3c6843afec7893644a649c87f
liyang <995720636@qq.com>
2016-04-15 15:07:44 +0800

    Initial Commit

---
+++ GG/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GG
+//
+//  Created by liyang on 16/4/15.
+//  Copyright © 2016年 liyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
